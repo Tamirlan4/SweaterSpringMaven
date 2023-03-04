fc44ad83d19c59bae58731843b251056c4fde584
Tamirlan4 <tamirlan_agubaev@mail.ru>
2023-03-04 11:41:00 +0600

    Initial commit

---
+++ src/main/java/com/example/sweaterspringmaven/SweaterSpringMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.sweaterspringmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweaterSpringMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweaterSpringMavenApplication.class, args);
+	}
+
+}
